89635c4905116f6ac298a4a43cb25d8cb952a760
Bùi Minh Thông <thonv7776@gmail.com>
2026-06-26 12:34:47 +0700

    cw__web1.4

--- templates/question.html.php
+++ templates/question.html.php
@@ -1,20 +1,20 @@
 <?php foreach($questions as $question): ?>
     <blockquote>
-        <h3><?=htmlspecialchars($question['title'], ENT_QUOTES, 'UTF-8')?></h3>
-        <p><?=htmlspecialchars($question['content'], ENT_QUOTES, 'UTF-8')?></p>
 
-        <small>
-            (by <a href="mailto:<?=htmlspecialchars($question['email'], ENT_QUOTES, 'UTF-8');?>">
-            <?=htmlspecialchars($question['username'], ENT_QUOTES, 'UTF-8');?></a>) 
-            | Module: <?=htmlspecialchars($question['module_name'], ENT_QUOTES, 'UTF-8');?>
-        </small>
-        <br><br>
+        <strong><?=htmlspecialchars($question['title'], ENT_QUOTES, 'UTF-8')?></strong><br><br>
+        
+        <?=htmlspecialchars($question['content'], ENT_QUOTES, 'UTF-8')?><br><br>
+
+        (by <a href="mailto:<?=htmlspecialchars($question['email'], ENT_QUOTES, 'UTF-8');?>">
+        <?=htmlspecialchars($question['username'], ENT_QUOTES, 'UTF-8');?></a>)<br><br>
+
+        <a href="editquestion.php?id=<?=$question['question_id']?>">Edit</a><br><br>
 
         <?php if (!empty($question['image'])): ?>
             <img height="100px" src="images/<?=htmlspecialchars($question['image'], ENT_QUOTES, 'UTF-8'); ?>" /><br><br>
         <?php endif; ?>
 
-        <a href="editquestion.php?id=<?=$question['question_id']?>">Edit</a><br><br>
+        (<?=htmlspecialchars($question['module_name'], ENT_QUOTES, 'UTF-8');?> Module)
 
         <form action="deletequestion.php" method="post">
             <input type="hidden" name="id" value="<?=$question['question_id']?>">
